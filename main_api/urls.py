bc490fc82df60ee6c6d156282bef0ead20659593
Nikhil Yadav <nanashi@Nikhils-Mini.bbrouter>
2025-12-03 18:14:43 +0530

    added host

--- main_api/urls.py
+++ main_api/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main_api.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/',home),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('Scraping_Event.urls')),
 ]
